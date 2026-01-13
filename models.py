3816ba63ddf0f1fd1d932137fe3071252b700fb4
David <davidkuron1996@gmail.com>
2026-01-13 09:10:02 +0300

    add this files

--- models.py
+++ models.py
@@ -18,3 +18,26 @@ class Episode(db.Model):
         if include_appearances:
             data['appearances'] = [appearance.to_dict() for appearance in self.appearances]
         return data
+class Guest(db.Model):
+    __tablename__ = 'guests'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    occupation = db.Column(db.String(100), nullable=False)
+    appearances = db.relationship('Appearance', backref='guest', cascade='all, delete-orphan')
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'occupation': self.occupation
+        }
+
+class Appearance(db.Model):
+    __tablename__ = 'appearances'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer, nullable=False)
+    episode_id = db.Column(db.Integer, db.ForeignKey('episodes.id'), nullable=False)
+    guest_id = db.Column(db.Integer, db.ForeignKey('guests.id'), nullable=False)
+    
