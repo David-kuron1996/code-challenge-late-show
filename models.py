e7ff379ef9b3796fa285e29997de05a44c2257db
David <davidkuron1996@gmail.com>
2026-01-13 10:57:57 +0300

    add this files

--- models.py
+++ models.py
@@ -1,29 +1,29 @@
-from app import db
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+
+db = SQLAlchemy()
 
 class Episode(db.Model):
     __tablename__ = 'episodes'
     
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.String(50), nullable=False)
+    date = db.Column(db.String(10), nullable=False)
     number = db.Column(db.Integer, nullable=False)
     appearances = db.relationship('Appearance', backref='episode', cascade='all, delete-orphan')
     
-    def to_dict(self, include_appearances=False):
-        data = {
+    def to_dict(self):
+        return {
             'id': self.id,
             'date': self.date,
             'number': self.number
         }
-        if include_appearances:
-            data['appearances'] = [appearance.to_dict() for appearance in self.appearances]
-        return data
+
 class Guest(db.Model):
     __tablename__ = 'guests'
     
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    occupation = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(80), nullable=False)
+    occupation = db.Column(db.String(80), nullable=False)
     appearances = db.relationship('Appearance', backref='guest', cascade='all, delete-orphan')
     
     def to_dict(self):
@@ -41,3 +41,26 @@ class Appearance(db.Model):
     episode_id = db.Column(db.Integer, db.ForeignKey('episodes.id'), nullable=False)
     guest_id = db.Column(db.Integer, db.ForeignKey('guests.id'), nullable=False)
     
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'rating': self.rating,
+            'episode_id': self.episode_id,
+            'guest_id': self.guest_id,
+            'episode': {
+                'id': self.episode.id,
+                'date': self.episode.date,
+                'number': self.episode.number
+            },
+            'guest': {
+                'id': self.guest.id,
+                'name': self.guest.name,
+                'occupation': self.guest.occupation
+            }
+        }
+    
+    def validate(self):
+        errors = []
+        if self.rating < 1 or self.rating > 5:
+            errors.append('Rating must be between 1 and 5')
+        return errors
